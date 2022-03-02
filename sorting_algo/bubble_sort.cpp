6c9d2bc8ba12f19f503389ced981d7243c6e6cbc
Neha0512 <78rakes@gmail.com>
2022-03-02 23:57:41 +0530

    updated

---
+++ sorting_algo/bubble_sort.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int bubbleSort(int arr[], int n)
+ {
+ 	for(int i=0; i<n-1; i++)
+ 	{
+ 		for(int j=i+1; j<n; j++)
+ 		{
+ 			if
+		 }
+	 }
+ }
---
+++ sorting_algo/selection_sort.cpp
@@ -0,0 +1,25 @@
+//use case: when array size is small
+
+#include<bits/stdc++.h>
+using namespace std;
+int selectSort(int arr[], int n)
+{
+	for(int i=0; i<n-1; i++)
+	{
+		int minInd=i;
+		for(int j=i+1; j<n;j++)
+		{
+			if(arr[j]<arr[minInd])
+			minInd=j;
+		}
+		swap(arr[minInd],arr[i]);
+	}
+	
+ } 
+ 
+ int main()
+ {
+ 	int n=5;
+ 	int arr[n]={6,8,5,10,3};
+ 	cout<<selectSort(arr, n);
+ }
